79e6760ff143810d0b3bc62733dfd0b1b3e9de80
98bazsikaX <63373205+98bazsikaX@users.noreply.github.com>
2020-11-22 19:05:00 +0100

    Remélem ezután megy mert ha nem én kiégek

--- create_poll.php
+++ create_poll.php
@@ -8,24 +8,60 @@
     <title>Szavazás létrehozása</title>
 </head>
 <body>
+    <!-- Első php include-->
 <?php
 require_once("scripts/php/header.php");
 name("Szavazás létrehozása");
-?>
-<div id="basic_details">
-<label>A szavazás neve:</label>
-<input type="text" id="name">
-<button onclick="createForm()" id="make_button">Forma létrehozása</button>
+?><!-- Első php include vége-->
+<div>
+    <form id="options" action="create_poll.php" method="post" enctype="multiplatform/data">
+        <div id="basic_details">
+            <label id="label_for_name">A szavazás neve:</label>
+            <input type="text" id="name" name="name" maxlength="16">
+            <br>
+            <label id="label_for_nr">Opciók száma</label>
+            <input type="number" id="nr_of_options" name="nr_of_options" value="0">
+            <br>
+            <button onclick="createForm()" id="make_button">Forma létrehozása</button>
+            <!-- <button onclick="add_option()" class="add_button" disabled>Opció hozzáadása</button> TODO: Megcsinálni h a többinél is legyen törlés (ha lehet ilyet) -->
+        </div>
+    <div id="geninputs">
+        <!-- Generált inputok jönnek ide-->
+    </div>
+   <!--  <button onclick="add_option()" class="add_button" disabled>Opció hozzáadása</button>  TODO: Ugyanaz mint fent-->
+   <div id="settings">
+   <label>Privát szavazás:</label>
+    <input type="radio" name="private" id="true">
+    <label for="true">Igen</label>
+    <br>
+    <input type="radio" name="private" id="false">
+    <label for="false">Nem</label>
+   </div>
+    <input type="reset" name="rst" id="reset" disabled>
+    <input type="submit" name="submit_poll" id="submitbutton" value="Szavazás létrehozása" disabled>
+    
+    </form>
 </div>
+<!-- TODO: MEGCSINÁLNI AZT H ELKÜLDJE A FORMOT!!!!!!!!!!!!!-->
+
+<?php
+
+if(isset($_POST['submit_poll'])){
+    $asd = $_POST['name'] . "   Első opció: " . $_POST['option_1'] . " Opciók száma: " . $_POST['nr_of_options'];
+    header("Location: /errorpage.php?hiba=Sikeresen létrehozta az adatbázist! $asd ");
+}
+
+
+
+?>
 
 
 
 
-
-
-
+<!-- Második php include footernek-->
 <?php
 require_once("scripts/php/footer.php");
 ?>
+<!-- Második php include vége-->
 </body>
 </html>
--- scripts/javascript/create_poll.js
+++ scripts/javascript/create_poll.js
@@ -1,4 +1,129 @@
 function createForm(){
     document.getElementById("make_button").disabled = true;
-    console.log(document.getElementById("name").value);
+    document.querySelectorAll('.add_button').forEach(function(button){
+        //console.log("ENTERED ! ");
+        button.disabled = false;
+       //button.style.display = "none";
+    });
+    //console.log(document.getElementById("name").value);
+    document.getElementById("nr_of_options").disabled = true;
+    options = parseInt(document.getElementById("nr_of_options").value); /*Globális scopeba kell hogy legyen*/
+    max_option = options;
+    console.log(options);
+    document.getElementById('submitbutton').disabled = false;
+    document.getElementById('reset').disabled = false;
+    if(options>16){
+        alert("Maximum 16 opciót adhat hozzá egy szavazáshoz, ön " + options + " adott meg ezért 16-ra lett állítva a száma");
+        options = 16;
+    }
+    for(let i = 0;i<options;++i){
+
+        /*label létrehozása*/
+        let text = document.createElement('label');
+        let name = (i+1) + ". opció: "
+        let id = "option_"+(i+1);
+        text.innerHTML = name + "  ";
+        text.classList.add(id);
+        //console.log(text);        
+        document.getElementById("geninputs").appendChild(text);
+
+        /*szövegmező létrehozása*/
+        let input = document.createElement('P');
+        console.log('input');
+       /*  input.classList.add(id);
+        input.setAttribute('type','text');
+        input.setAttribute('maxlength','16');
+        input.setAttribute('name',id);
+        //input.name = id;
+        console.log(input.getAttribute('name'));
+        input.required = true; */
+        //let input =document.createElement(`<input type="text" name="${id}" value="${i + 1}" maxlength="16" class="${id}" required>`);
+
+        document.getElementById("geninputs").appendChild(input);
+
+        /*törlés gomb létrehozása ha 2-nél több opció van*/
+        if(options>2){
+            let remove = document.createElement('button');
+            remove.classList.add(id);
+            remove.classList.add("remove_option");
+            remove.innerText = name + " törlése";
+            document.getElementById("geninputs").appendChild(remove);
+            remove.setAttribute("onclick","delete_option('"+id+"')");
+        }
+
+        /*sortörés*/
+        let br = document.createElement('br');
+        br.classList.add(id);
+        document.getElementById("geninputs").appendChild(br);
+    }
+}
+/* function add_option(){ TODO: Megcsinálni hogy működjön a törlés a többinél!
+    if(options<3){
+
+    }
+    ++options;
+    let lbl = document.createElement('label');
+    lbl.innerHTML = options + ". opció: ";
+    lbl.classList.add("option_"+options);
+    document.getElementById("geninputs").appendChild(lbl);
+
+    let input = document.createElement('input');
+    input.classList.add("option_"+options);
+    input.setAttribute('type','text');
+    input.setAttribute('maxlength','16');
+    input.required = true;
+    document.getElementById("geninputs").appendChild(input);
+
+    let remove = document.createElement('button');
+    remove.classList.add("option_"+options);
+    remove.classList.add("remove_option");
+    remove.innerText = options + ". elem törlése";
+    document.getElementById("geninputs").appendChild(remove);
+    remove.setAttribute("onclick","delete_option('option_"+options+"')");
+
+
+    let br = document.createElement('br');
+    br.classList.add("option_"+options);
+    document.getElementById("geninputs").appendChild(br);
+}
+ */
+function delete_option(id){
+    /*
+    TODO: VALAMI FOS GECI AZT OKOZZA HOGY HA KISEBB 3-nál AZ ÖSSZ LEHETŐSÉGEK SZÁMA AKKOR KURVÁRA KITÖRÖL MINDENT (IS)
+    */
+    document.querySelectorAll("."+id).forEach(function(element){
+        element.remove();
+    });
+    --options;
+    document.getElementById("nr_of_options").value = options;
+
+
+    let current  = parseInt(id.split('_')[1]);
+    for(;current<(options+1);++current){
+        document.querySelectorAll(".option_"+(current+1)).forEach(function(e){
+            e.classList.remove("option_"+(current+1))
+            e.classList.add("option_"+current);
+            if(e.tagName == 'INPUT'){
+                e.setAttribute('name',"option_"+current);
+                e.name = "option_"+current;
+            }
+            if(e.tagName == 'LABEL' || e.tagName == 'BUTTON'){
+                e.innerHTML = current + ". opció";
+                if(e.tagName == 'BUTTON'){
+                    e.innerHTML += " törlése";
+                }else if(e.tagName == 'LABEL'){
+                    e.innerHTML += ": ";
+                }
+            }
+        });
+    }
+    --max_option;
+    //console.log(options);
+    if(options < 3){
+        //console.log("belépett az ifbe");
+        document.querySelectorAll('.remove_option').forEach(function(button){
+            button.remove();
+        });
+    }
+
 }
